ac023d38545ba1d60e1526c8e96e23d915760aaa
AnmolSinghNegi1 <anmolsinghnegi098@gmail.com>
2024-06-09 12:42:05 +0530

    initial commit

---
+++ src/Data/Teachersdata.jsx
@@ -0,0 +1,73 @@
+const data = [
+    {
+       id : 1,
+       name : "Bhupinder Singh ",
+       Qualification : "MSC Applied Physics MSC Computer Science B.E.d" ,
+       Address : "92/4 preet nagar ladowali road jalandhar", 
+       Contact : "9815888542",
+       Experience : "15 years of experience of teaching physics"
+    },
+    {
+        id : 2,
+        name : "Achievers Academy",
+        Qualification : "B.TECH IT" ,
+        Address : "284/5,Dakoha,RamaMandi, Jalandhar", 
+        Contact  : "8427874468 "
+ 
+     },
+     {
+        id : 3,
+        name : "Success Point",
+        Qualification : "B.TECH IT" ,
+        Address : "H.NO-284/5,Sainik Vihar,RamaMandi, Jalandhar", 
+        Experience : "5 years in BSF School "
+ 
+     },
+     {
+        id : 4,
+        name : "Amrit Academy ",
+        Qualification : "B.TECH IT" ,
+        Address : "H.NO-284/5,Sainik Vihar,RamaMandi, Jalandhar", 
+        Experience : "5 years in BSF School "
+ 
+     },
+     {
+        id : 5,
+        name : "Tutor Point ",
+        Qualification : "BSC COMPUTER SCIENCE  MSC CHEMISTRY " ,
+        Address : "H.NO-284/5,Sainik Vihar,RamaMandi, Jalandhar", 
+        Experience : "10 years Experience of teaching physics and chemistry ",
+        image : "sample.jpg",
+     },
+     {
+        id : 6,
+        name : "Anmol",
+        Qualification : "B.TECH IT" ,
+        Address : "H.NO-284/5,Sainik Vihar,RamaMandi, Jalandhar", 
+        Experience : "5 years in BSF School "
+ 
+     },
+
+];
+// const fetch = require('node-fetch');
+
+// const url = 'https://comparable-companies.p.rapidapi.com/similar?domain=unilever.com&limit=50';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': '26a4053ddfmsh99c50643a9abecfp15c131jsn6afa28ef4f1c',
+//     'X-RapidAPI-Host': 'comparable-companies.p.rapidapi.com'
+//   }
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+
+export default data;
+
+
+
